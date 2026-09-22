ab3e274be283126a4f179b5aa836e6e8545035fd
Xwoder <Xwoder@qq.com>
2026-09-22 16:00:13 +0800

    feat: 新增 Camera 类

---
+++ Camera.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+from Point3 import Point3
+
+
+@dataclass(frozen=True)
+class Camera:
+    center: Point3 = Point3(0, 0, 0)
+
+    def __repr__(self) -> str:
+        return f"Camera(center={self._center!r})"
