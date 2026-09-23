8780029ca4017c06c633ea18bdb244922f6bd86f
Xwoder <Xwoder@qq.com>
2026-09-23 13:36:34 +0800

    feat(scene): 添加背景颜色属性

--- Scene.py
+++ Scene.py
@@ -1,13 +1,16 @@
+from Color import Color
 from Sphere import Sphere
 
 
 class Scene:
+    BACKGROUND_COLOR: Color = Color.Black
+
     _spheres: list[Sphere]
 
     def __init__(self, spheres: list[Sphere]):
         self._spheres = spheres
 
-    def add(self, s:Sphere):
+    def add(self, s: Sphere):
         self._spheres.append(s)
 
     @property
