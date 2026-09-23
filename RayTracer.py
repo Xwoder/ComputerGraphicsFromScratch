97f72b2dc244a66b4bfe2767ec23180d282236dc
Xwoder <Xwoder@qq.com>
2026-09-23 16:14:58 +0800

    refactor(RayTracer): 简化 traceRay 方法签名与逻辑

--- RayTracer.py
+++ RayTracer.py
@@ -4,7 +4,6 @@ from Color import Color
 from Interval import Interval
 from Ray import Ray
 from Scene import Scene
-from Sphere import Sphere
 
 
 class RayTracer:
@@ -13,20 +12,17 @@ class RayTracer:
     def __init__(self, scene: Scene):
         self._scene = scene
 
-    def traceRay(
-            self,
-            ray: Ray,
-            interval: Interval,
-    ) -> Color:
-        closest_t = +math.inf
-        closest_sphere: Sphere | None = None
+    def traceRay(self, ray: Ray) -> Color:
+        closest_t = math.inf
+        closest_sphere = None
 
         for sphere in self._scene.spheres:
             for t in sphere.intersect(ray):
-                if t in interval and t < closest_t:
-                    closest_t, closest_sphere = t, sphere
+                if t in Interval(1, math.inf) and t < closest_t:
+                    closest_t = t
+                    closest_sphere = sphere
 
-        if closest_sphere is not None:
-            return closest_sphere.color
-        else:
+        if closest_sphere is None:
             return self._scene.BACKGROUND_COLOR
+        else:
+            return closest_sphere.color
