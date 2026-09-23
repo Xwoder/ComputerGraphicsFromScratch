20f009688a49a90b8ac60084dad21be7ba67ec01
Xwoder <Xwoder@qq.com>
2026-09-23 10:35:43 +0800

    refactor(canvas): 重构像素访问方法命名

--- Canvas.py
+++ Canvas.py
@@ -5,18 +5,27 @@ class Canvas:
     _width: int = 800
     _height: int = 600
     _pixels: list[list[Color]]
+    __defaultColor: Color = Color.Black
 
-    def __init__(self, width: int, height: int) -> None:
+    def __init__(self,
+                 width: int,
+                 height: int) -> None:
         super().__init__()
 
         self._width = width
         self._height = height
-        self._pixels = [[Color(0, 0, 0) for _ in range(width)] for _ in range(height)]
+        self._pixels = [[self.__defaultColor for _ in range(width)]
+                        for _ in range(height)]
 
-    def set_pixel(self, x: int, y: int, color: Color) -> None:
+    def putPixel(self,
+                 x: int,
+                 y: int,
+                 color: Color) -> None:
         self._pixels[y][x] = color
 
-    def get_pixel(self, x: int, y: int) -> Color:
+    def getPixel(self,
+                 x: int,
+                 y: int) -> Color:
         return self._pixels[y][x]
 
     @property
