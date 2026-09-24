a5b283baa069b124b67b24a836bf8b1c13dcb932
Xwoder <Xwoder@qq.com>
2026-09-24 14:55:38 +0800

    feat: 为球体添加高光参数

--- main.py
+++ main.py
@@ -34,10 +34,10 @@ if __name__ == '__main__':
     camera: Camera = Camera()
 
     spheres: list[Sphere] = [
-        Sphere(center=Point3(0, -1, 3), radius=1, color=Color(255, 0, 0)),
-        Sphere(center=Point3(2, 0, 4), radius=1, color=Color(0, 0, 255)),
-        Sphere(center=Point3(-2, 0, 4), radius=1, color=Color(0, 255, 0)),
-        Sphere(center=Point3(0, -5001, 0), radius=5000, color=Color(255, 255, 0)),
+        Sphere(center=Point3(0, -1, 3), radius=1, color=Color(255, 0, 0), specular=500),
+        Sphere(center=Point3(2, 0, 4), radius=1, color=Color(0, 0, 255), specular=500),
+        Sphere(center=Point3(-2, 0, 4), radius=1, color=Color(0, 255, 0), specular=10),
+        Sphere(center=Point3(0, -5001, 0), radius=5000, color=Color(255, 255, 0), specular=1000),
     ]
     lights: list[Light] = [
         AmbientLight(0.2),
