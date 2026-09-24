a958e0b55aab8c97d817a1467d7631773f60e329
Xwoder <Xwoder@qq.com>
2026-09-24 17:04:00 +0800

    feat(场景): 添加最近交点计算方法

--- Scene.py
+++ Scene.py
@@ -1,7 +1,11 @@
+import math
 from dataclasses import dataclass, field
 
 from Color import Color, WHITE
+from Interval import Interval
 from Light.Light import Light
+from Number import Number
+from Ray import Ray
 from Sphere import Sphere
 
 
@@ -15,3 +19,20 @@ class Scene:
     lights: list[Light] = field(default_factory=list)
 
     BACKGROUND_COLOR: Color = WHITE
+
+    def closestIntersection(self, ray: Ray, interval: Interval) -> tuple[Sphere | None, Number]:
+        closest_t = math.inf
+        closest_sphere = None
+
+        for sphere in self.spheres:
+            t1, t2 = sphere.intersect(ray)
+
+            if interval.min <= t1 <= Interval.max and t1 < closest_t:
+                closest_t = t1
+                closest_sphere = sphere
+
+            if interval.min <= t2 <= interval.max and t2 < closest_t:
+                closest_t = t2
+                closest_sphere = sphere
+
+        return closest_sphere, closest_t
